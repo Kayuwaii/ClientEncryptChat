3d2e10ef315c96dcb863c1848cc76daee8e07e1c
ALEX CORONAS ALTEMIR <alexcoralt@monlau.es>
2017-10-30 21:50:33 +0100

    Encryption messagiung partially implemented and connection.

--- ClientEncryptChat/Program.cs
+++ ClientEncryptChat/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,107 @@ namespace ClientEncryptChat
     {
         static void Main(string[] args)
         {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("192.168.1.12", 8001);
+                // use the ipaddress as in the server program
+
+                String EncryptionKey = GetHashedKey("192.168.1.12");
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                String str = TxtEncrypt(Console.ReadLine(), EncryptionKey);
+                Stream stm = tcpclnt.GetStream();
+
+                byte[] ba = Encoding.UTF8.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[ba.Length];
+                int k = stm.Read(bb, 0, ba.Length);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+            Console.ReadLine();
+        }
+
+        public static string GetHashedKey(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            int cntr = 0;
+            foreach (byte x in hash)
+            {
+                if (cntr == 1)
+                {
+                    cntr = 0;
+                }
+                else
+                {
+                    hashString += String.Format("{0:x2}", x);
+                    cntr++;
+                }
+            }
+            return hashString;
+        }
+
+        //Encrypting a string
+        public static string TxtEncrypt(string inText, string key)
+        {
+            byte[] bytesBuff = Encoding.UTF8.GetBytes(inText);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes crypto = new Rfc2898DeriveBytes(key, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                aes.Key = crypto.GetBytes(32);
+                aes.IV = crypto.GetBytes(16);
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                        cStream.Close();
+                    }
+                    inText = Convert.ToBase64String(mStream.ToArray());
+                }
+            }
+            return inText;
+        }
+
+        //Decrypting a string
+        public static string TxtDecrypt(string cryptTxt, string key)
+        {
+            cryptTxt = cryptTxt.Replace(" ", "+");
+            byte[] bytesBuff = Convert.FromBase64String(cryptTxt);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes crypto = new Rfc2898DeriveBytes(key, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                aes.Key = crypto.GetBytes(32);
+                aes.IV = crypto.GetBytes(16);
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                        cStream.Close();
+                    }
+                    cryptTxt = Encoding.UTF8.GetString(mStream.ToArray());
+                }
+            }
+            return cryptTxt;
         }
     }
 }
